5cc0ce91e861a7ed20c6b3380f2baa489166fe83
Teo <nastase.teo34@gmail.com>
2024-11-18 12:09:05 +0200

    I've started to write for the DFA from Regex app, that will simulate an DFA Automaton

---
+++ DeterministicFiniteAutomaton.cpp
@@ -0,0 +1,48 @@
+﻿#include "DeterministicFiniteAutomaton.h"
+
+DeterministicFiniteAutomaton::DeterministicFiniteAutomaton(const std::set<std::string> states, const std::set<std::string> alphabet, const std::map<std::pair<std::string, std::string>, std::string> transitionFunction, const std::string initialState, const std::set<std::string> finalStates)
+	:states(states),alphabet(alphabet),transitionFunction(transitionFunction),initialState(initialState),finalStates(finalStates)
+{
+}
+
+bool DeterministicFiniteAutomaton::VerifyAutomaton()
+{
+	if (states.find(initialState) == states.end())
+		return false; // Starea initiala nevalida
+
+	for (const auto& state : finalStates) {
+		if (states.find(state) == states.end())
+			return false; // Starile finale nevalide
+	}
+
+	if (alphabet.empty())
+		return false; // Alfabet gol
+
+	for (const auto& transition : transitionFunction) {
+		const auto& key = transition.first;
+		const auto& resultState = transition.second;
+
+		const std::string& currentState = key.first;
+		const std::string& symbol = key.second;
+
+		if (states.find(currentState) == states.end()) {
+			return false; // Stare invalida
+		}
+
+		if (alphabet.find(symbol) == alphabet.end()) {
+			return false; // Simbol invalid
+		}
+
+		if (states.find(resultState) == states.end()) {
+			return false; // Tranzitie spre o stare invalida
+		}
+	}
+
+	if (states.empty())
+		return false; // Nu exista stari
+
+	if (finalStates.empty())
+		return false; // Nu exista stari finale
+
+	return true;
+}
---
+++ DeterministicFiniteAutomaton.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+#include<set>
+#include<string>
+#include<map>
+class DeterministicFiniteAutomaton
+{
+private:
+	const std::set<std::string> states;//Q
+	const std::set<std::string> alphabet;//Σ
+	const std::map<std::pair<std::string, std::string>, std::string> transitionFunction;//δ
+	const std::string initialState;//q0
+	const std::set<std::string> finalStates;//F
+
+public:
+
+	DeterministicFiniteAutomaton(const std::set<std::string> states, const std::set<std::string> alphabet,
+		const std::map<std::pair<std::string, std::string>, std::string> transitionFunction,
+		const std::string initialState,
+		const std::set<std::string> finalStates);
+
+	bool VerifyAutomaton();
+
+};
+
